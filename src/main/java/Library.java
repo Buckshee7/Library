1397e19449eb91e795566d1bdc77745d431f4dde
Timothy Saudners <timsaunders88@gmail.com>
2020-09-22 19:32:58 +0100

    initial commit. book and library classes with constructors created

---
+++ src/main/java/Library.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Library {
+
+    private ArrayList<Book> catalogue;
+    private int capacity;
+
+    public Library (int capacity){
+        this.capacity = capacity;
+    }
+
+}
